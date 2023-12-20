8c81a484892a35b9f9f431b19c8d43c8e9dc1c7c
tshaax <tshaax@gmail.com>
2023-12-20 03:32:10 +0200

    added repository

---
+++ PayScale.DataAccess/Repository/IRepository/ITaxTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using PayScale.DataAccess.Repository.IRepository;
+using PayScale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayScale.DataAccess.Repository.IRepository
+{
+    public interface ITaxTypeRepository : IRepository<TaxType>
+    {
+    }
+}
