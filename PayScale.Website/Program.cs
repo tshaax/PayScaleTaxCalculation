e7f82bc73e41bbc8f04c041f5b608dc0c8727535
tshaax <tshaax@gmail.com>
2024-01-02 20:15:26 +0200

    removed comments

--- PayScale.Website/Program.cs
+++ PayScale.Website/Program.cs
@@ -18,13 +18,6 @@ builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
 app.UseExceptionHandler("/Home/Error");
 app.UseHsts();
 app.UseStatusCodePages(Text.Plain, "Ooopps!! Something went wrong with your input, please try again. Error code: {0}");
